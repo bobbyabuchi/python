ba185c8903c73ae4685e1a1731fe12cc46fd90a8
Bobby ABUCHI <bobbyabuchi@gmail.com>
2024-07-09 22:12:33 +0100

    Update pythonNote24.py

--- pythonNote24.py
+++ pythonNote24.py
@@ -42,3 +42,48 @@ print(latinScore) # 96
 print(result)
 # print(editedResult)
 print(list(goodRange))
+
+# date: 9.7.24
+
+# Len's Slice: organize some of your sales data
+
+toppings = ["pepperoni","pineapple","cheese","sausage","olives","anchovies","mushrooms"]
+prices = [2,6,1,3,2,7,2]
+
+num_two_dollar_slices = prices.count(2)
+num_pizzas = len(toppings)
+
+pizza_and_prices = [
+  [2, "pepperoni"],
+  [6, "pineapple"],
+  [1, "cheese"],
+  [3, "sausage"],
+  [2, "olives"],
+  [7, "anchovias"],
+  [2, "mushrooms"],
+]
+
+# Sort in the order of increasing price (asc)
+pizza_and_prices = sorted(pizza_and_prices)
+cheapest_pizza = pizza_and_prices[0]
+priciest_pizza = pizza_and_prices[-1]
+
+# most expensive sold
+pizza_and_prices.pop() 
+
+# add new product
+newPizaa = [2.5,"peppers"]
+pizza_and_prices = pizza_and_prices + newPizaa
+
+# get the three cheapest pizza
+three_cheapest = pizza_and_prices[:3]
+
+# print(num_two_dollar_slices)
+# print(num_pizzas)
+print("We sell",num_pizzas,"different kinds of pizza!\n")
+print(pizza_and_prices,"\n")
+print(priciest_pizza,cheapest_pizza,"\n")
+print(three_cheapest,"\n")
+
+
+
